c53f847975546d1f446ebe9ba70d64d10554cdde
Polina Skrobot <chi.polly.no@gmail.com>
2022-06-23 23:03:04 -0700

    Initial

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const Twit = require("twit");
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "*");
+//   next();
+// });
+//console.log(process.env);
+const T = new Twit({
+  consumer_key: process.env.TWEETER_API_KEY,
+  consumer_secret: process.env.TWEETER_API_SECRET,
+  access_token: process.env.TWEETER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWEETER_ACCESS_TOKEN_SECRET,
+});
+
+app.get("/", (req, res) => {
+  T.get(
+    "search/tweets",
+    { q: req.body || "banana", count: 10 },
+    function (err, data, response) {
+      console.log(data.statuses);
+      res.data;
+    }
+  );
+});
+const port = 3001;
+
+app.listen(port, () => {
+  console.log("Server is listening on " + port);
+});
