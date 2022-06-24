230fa1266321c7f274ccd033c6b728f630a4e167
Polina Skrobot <chi.polly.no@gmail.com>
2022-06-23 23:19:44 -0700

    port change

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get("/", (req, res) => {
     }
   );
 });
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log("Server is listening on " + port);
